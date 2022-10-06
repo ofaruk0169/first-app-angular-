1e0d3ef2990ef0d2da61fe49f9563cab36b3250d
Omare Faruk <ofaruk0169@gmail.com>
2022-10-06 17:22:24 +0100

    added new components for error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    WarningAlertComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
